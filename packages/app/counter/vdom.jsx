94df876498acda1204023c63a057a982cc0c99d1
patrick-salvatore <salvatorepat8@gmail.com>
2023-03-20 22:03:29 -0400

    restructure dirs

--- packages/app/counter/vdom.jsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@lnl/framework/vdom";
-
-export const Static = () => {
-  return <div class="card">Hello World</div>;
-};
-
-render(<Static />, document.getElementById("app"));
--- packages/vanilla/hacker_news/pages/users.data.ts
+++
@@ -1,8 +0,0 @@
-import fetchAPI from "@lnl/lib";
-
-export default async function data({ params: { path } }) {
-  const id = path.split("/")[path.split("/").length - 1];
-  const data = await fetchAPI(`user/${id}`);
-
-  return { user: data };
-}
